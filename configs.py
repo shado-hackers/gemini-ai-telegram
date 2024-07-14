2f8e029c46b5dd0e2f907c535bab2ad11f513473
NOTHING <70184956+shado-hackers@users.noreply.github.com>
2024-07-14 08:10:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,4 +7,4 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    
+    API_KEY = os.environ.get("API_KEY", "")
